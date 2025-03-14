303b894ec8e4ed738cefde26084ccd9dee536177
xjn-La-La-land <xiejianan21@mails.ucas.ac.cn>
2025-03-14 11:05:04 +0800

    modified instr-test.c

--- rvxtm-test/instr-test.c
+++ rvxtm-test/instr-test.c
@@ -10,45 +10,48 @@ int mem_data = 0x12345678;
 //   0x0c000a0b
 // };
 
-void _start() {
-  asm volatile (
-    "li t0, 0x9abc\n\t"   // some regular riscv instrs
-    "sub t1, t0, t0\n\t"
-    "lw s1, 48(%0)\n\t"
-    "xor t2, t1, t0\n\t"
-    "or t3, t2, t0\n\t"
-    "and t4, t3, t0\n\t"
-    "sw s0, 48(%0)\n\t"
-    "sll t5, t4, 2\n\t"
-    "srl t6, t5, 1\n\t"
+__attribute__((optimize("O0"))) void test_wrapper() {
+  asm volatile("li t0, 0x9abc\n\t" // some regular riscv instrs
+               "sub t1, t0, t0\n\t"
+               "lw s1, 48(%0)\n\t"
+               "xor t2, t1, t0\n\t"
+               "or t3, t2, t0\n\t"
+               "and t4, t3, t0\n\t"
+               "sw s0, 48(%0)\n\t"
+               "sll t5, t4, 2\n\t"
+               "srl t6, t5, 1\n\t"
 
-    ".insn 0x01d6420b\n\t"  // tileloadd tmm4, 0(x12), x29
+               ".insn 0x01d6420b\n\t" // tileloadd tmm4, 0(x12), x29
 
-    "li t0, 0x1234\n\t"
-    "sub t2, t1, t0\n\t"
-    "lw s1, 16(%0)\n\t"
-    "xor t3, t2, t0\n\t"
-    "or t4, t3, t0\n\t"
-    "and t5, t4, t0\n\t"
-    "sw s0, 16(%0)\n\t"
-    "sll t6, t5, 2\n\t"
-    "addi t1, t0, -1\n\t"
+               "li t0, 0x1234\n\t"
+               "sub t2, t1, t0\n\t"
+               "lw s1, 16(%0)\n\t"
+               "xor t3, t2, t0\n\t"
+               "or t4, t3, t0\n\t"
+               "and t5, t4, t0\n\t"
+               "sw s0, 16(%0)\n\t"
+               "sll t6, t5, 2\n\t"
+               "addi t1, t0, -1\n\t"
 
-    ".insn 0x01d6620b\n\t" // tilestored tmm4, 0(x12), x29
-  
-    "li t0, 0x5678\n\t"
-    "add t1, t0, t0\n\t"
-    "lw s1, 32(%0)\n\t"
-    "xor t2, t1, t0\n\t"
-    "or t3, t2, t0\n\t"
-    "and t4, t3, t0\n\t"
-    "sw s0, 32(%0)\n\t"
-    "sll t5, t4, 2\n\t"
-    "srl t6, t5, 1\n\t"
+               ".insn 0x01d6620b\n\t" // tilestored tmm4, 0(x12), x29
 
-    ".insn 0x00000a0b\n\t" // tdpbssd tmm4, tmm2, tmm0
-    :
-    : "r"(&mem_data)
-    : "memory", "t0", "t1", "t2", "t3", "t4", "t5", "t6"
-  );
+               "li t0, 0x5678\n\t"
+               "add t1, t0, t0\n\t"
+               "lw s1, 32(%0)\n\t"
+               "xor t2, t1, t0\n\t"
+               "or t3, t2, t0\n\t"
+               "and t4, t3, t0\n\t"
+               "sw s0, 32(%0)\n\t"
+               "sll t5, t4, 2\n\t"
+               "srl t6, t5, 1\n\t"
+
+               ".insn 0x00000a0b\n\t" // tdpbssd tmm4, tmm2, tmm0
+               :
+               : "r"(&mem_data)
+               : "memory", "t0", "t1", "t2", "t3", "t4", "t5", "t6");
+}
+
+int main() {
+  test_wrapper();
+  return 0;
 }
